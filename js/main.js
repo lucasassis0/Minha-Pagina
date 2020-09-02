33f5efd9f455b621f9fd21a938f7c38bb35a2eb9
Lucas Assis <lucas.assis_@hotmail.com>
2020-09-02 18:44:12 -0400

    adicionando bootstrap

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 function consultaCep() {
+    $(".barra-progresso").show()
     var cep = document.getElementById("cep").value;
     if (cep) {
         var url = `http://viacep.com.br/ws/${cep}/json/`
@@ -15,9 +16,17 @@ function consultaCep() {
                 // document.getElementById("localidade").innerHTML = res.localidade
                 $("#uf").html(res.uf)
                 // document.getElementById("uf").innerHTML = res.uf
+                $("#titulo_cep").html(`CEP ${res.cep}`)
+                $(".cep").show()
+                $(".barra-progresso").hide()
             }
         })
     } else {
         alert("Favor inserir o cep!")
     }
 }
+
+$(()=>{
+    $(".cep").hide()
+    $(".barra-progresso").hide()
+})
